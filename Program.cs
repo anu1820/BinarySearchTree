a302e84a1f250384d9dc36ca9cee82b0adb455b2
anu1820 <anurajpatil35@gmail.com>
2023-01-13 23:32:23 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -5,6 +5,19 @@ namespace BinarySearchTree
     {
         public static void Main(string[] args)
         {
+            BinaryTree bn = new BinaryTree();
+            bn.Add(30);
+            bn.Add(40);
+            bn.Add(50);
+            bn.Add(60);
+            bn.Add(80);
+            Console.WriteLine("After Preorder Result Will be: ");
+            bn.TravesrePreorder(bn.root);
+            Console.WriteLine("After Postorder Result Will be: ");
+            bn.TravesrePostorder(bn.root);
+            Console.WriteLine("After Inorder Result Will be: ");
+            bn.TravesreInorder(bn.root);
+
             Console.WriteLine(" Welcome to Binary Search Tree Program ");
 
         }
