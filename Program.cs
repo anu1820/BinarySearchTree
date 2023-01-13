fab09238e79cc6286544d5940187e0c59d0e00c8
anu1820 <anurajpatil35@gmail.com>
2023-01-13 23:16:25 +0530

    Greeting message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Binary Search Tree Program ");
+
+        }
+
+    }
+}
